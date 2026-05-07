7d56c54d57303bc2dba557a00714c479ac806b13
SotaTamura <tamso1868@gmail.com>
2026-05-07 11:24:27 +0900

    unity移行

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -82,78 +82,94 @@ export default function Home({ id }: { id: string | undefined }) {
     }
 
     return (
-        <div className="h-full overflow-y-auto pb-10 relative">
-            <div className="loginBtn">
-                <p>{user?.name || t("ゲスト")}</p>
+        <>
+            <div className="bg-amber-400 text-3xl font-bold border-4 border-double border-black text-center p-10">
+                <div className="text-black">Unityに移行しました！</div>
+                <div>
+                    <a className="text-blue-500 underline" href="https://unityroom.com/games/block-lockd">
+                        https://unityroom.com/games/block-lockd
+                    </a>
+                </div>
+                <div>
+                    Xフォローお願いします:{" "}
+                    <a className="text-blue-500 underline" href="https://x.com/appleS000da2">
+                        https://x.com/appleS000da2
+                    </a>
+                </div>
             </div>
-            <Image src={"/logo.png"} className="mt-[20vh] h-[24vmin] m-auto" alt="logo" width={500} height={500} style={{ width: "auto", imageRendering: "pixelated" }} unoptimized />
-            <div className="grid grid-cols-2 gap-2 w-[50vmin] h-[50vmin] m-auto mt-[8vh]">
-                <Link className="btn home-btn flex flex-col items-center w-full h-full py-2" href={"/select-stage"}>
-                    <div className="grow flex items-center justify-center w-full">
-                        <div className="w-[70%] h-[70%] flex justify-center items-center">
-                            <RightSvg />
-                        </div>
-                    </div>
-                    <div className="whitespace-nowrap mb-1 text-black" style={{ fontSize: "3.5dvmin" }}>
-                        {t("プレイ")}
-                    </div>
-                </Link>
-                <Link className="btn home-btn flex flex-col items-center w-full h-full py-2" href={"/settings"}>
-                    <div className="grow flex items-center justify-center w-full">
-                        <div className="w-[70%] h-[70%] flex justify-center items-center">
-                            <GearSvg />
-                        </div>
-                    </div>
-                    <div className="whitespace-nowrap mb-1 text-black" style={{ fontSize: "3.5dvmin" }}>
-                        {t("設定")}
-                    </div>
-                </Link>
-                {user.id === "guest" ? (
-                    <div
-                        onClick={() => {
-                            showAlert(t("ステージを作成するには、「設定」からニックネームを入力してください。"));
-                        }}
-                        className="btn home-btn flex flex-col items-center w-full h-full py-2 cursor-pointer">
+            <div className="h-full overflow-y-auto pb-10 relative">
+                <div className="loginBtn">
+                    <p>{user?.name || t("ゲスト")}</p>
+                </div>
+                <Image src={"/logo.png"} className="mt-[20vh] h-[24vmin] m-auto" alt="logo" width={500} height={500} style={{ width: "auto", imageRendering: "pixelated" }} unoptimized />
+                <div className="grid grid-cols-2 gap-2 w-[50vmin] h-[50vmin] m-auto mt-[8vh]">
+                    <Link className="btn home-btn flex flex-col items-center w-full h-full py-2" href={"/select-stage"}>
                         <div className="grow flex items-center justify-center w-full">
                             <div className="w-[70%] h-[70%] flex justify-center items-center">
-                                <WrenchSvg />
+                                <RightSvg />
                             </div>
                         </div>
                         <div className="whitespace-nowrap mb-1 text-black" style={{ fontSize: "3.5dvmin" }}>
-                            {t("作成する")}
-                        </div>
-                    </div>
-                ) : (
-                    <Link href={"/editor"} className="btn home-btn flex flex-col items-center w-full h-full py-2">
-                        <div className="grow flex items-center justify-center w-full">
-                            <div className="w-[70%] h-[70%] flex justify-center items-center">
-                                <WrenchSvg />
-                            </div>
-                        </div>
-                        <div className="whitespace-nowrap mb-1 text-black" style={{ fontSize: "3.5dvmin" }}>
-                            {t("作成する")}
+                            {t("プレイ")}
                         </div>
                     </Link>
-                )}
-                <Link href={"/select-online-stage"} className="btn home-btn flex flex-col items-center w-full h-full py-2">
-                    <div className="grow flex items-center justify-center w-full">
-                        <div className="w-[70%] h-[70%] flex justify-center items-center">
-                            <WorldSvg />
+                    <Link className="btn home-btn flex flex-col items-center w-full h-full py-2" href={"/settings"}>
+                        <div className="grow flex items-center justify-center w-full">
+                            <div className="w-[70%] h-[70%] flex justify-center items-center">
+                                <GearSvg />
+                            </div>
                         </div>
-                    </div>
-                    <div className="whitespace-nowrap mb-1 text-black" style={{ fontSize: "3.5dvmin" }}>
-                        {t("オンライン")}
-                    </div>
-                </Link>
+                        <div className="whitespace-nowrap mb-1 text-black" style={{ fontSize: "3.5dvmin" }}>
+                            {t("設定")}
+                        </div>
+                    </Link>
+                    {user.id === "guest" ? (
+                        <div
+                            onClick={() => {
+                                showAlert(t("ステージを作成するには、「設定」からニックネームを入力してください。"));
+                            }}
+                            className="btn home-btn flex flex-col items-center w-full h-full py-2 cursor-pointer">
+                            <div className="grow flex items-center justify-center w-full">
+                                <div className="w-[70%] h-[70%] flex justify-center items-center">
+                                    <WrenchSvg />
+                                </div>
+                            </div>
+                            <div className="whitespace-nowrap mb-1 text-black" style={{ fontSize: "3.5dvmin" }}>
+                                {t("作成する")}
+                            </div>
+                        </div>
+                    ) : (
+                        <Link href={"/editor"} className="btn home-btn flex flex-col items-center w-full h-full py-2">
+                            <div className="grow flex items-center justify-center w-full">
+                                <div className="w-[70%] h-[70%] flex justify-center items-center">
+                                    <WrenchSvg />
+                                </div>
+                            </div>
+                            <div className="whitespace-nowrap mb-1 text-black" style={{ fontSize: "3.5dvmin" }}>
+                                {t("作成する")}
+                            </div>
+                        </Link>
+                    )}
+                    <Link href={"/select-online-stage"} className="btn home-btn flex flex-col items-center w-full h-full py-2">
+                        <div className="grow flex items-center justify-center w-full">
+                            <div className="w-[70%] h-[70%] flex justify-center items-center">
+                                <WorldSvg />
+                            </div>
+                        </div>
+                        <div className="whitespace-nowrap mb-1 text-black" style={{ fontSize: "3.5dvmin" }}>
+                            {t("オンライン")}
+                        </div>
+                    </Link>
+                </div>
+                <div className="flex justify-center w-full mt-20 gap-7">
+                    <Link href="/terms" className="text-gray-300 underline">
+                        {t("利用規約")}
+                    </Link>
+                    <Link href="/credits" className="text-gray-300 underline">
+                        {t("クレジット")}
+                    </Link>
+                </div>
             </div>
-            <div className="flex justify-center w-full mt-20 gap-7">
-                <Link href="/terms" className="text-gray-300 underline">
-                    {t("利用規約")}
-                </Link>
-                <Link href="/credits" className="text-gray-300 underline">
-                    {t("クレジット")}
-                </Link>
-            </div>
-        </div>
+        </>
     );
 }
